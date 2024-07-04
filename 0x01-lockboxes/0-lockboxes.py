c829e1c15eb4defa6370c3a8e4c0664a22d20c15
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-05 00:31:06 +0200

    Function that determines if all the boxes can be opened.

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+Function that determines if all the boxes can be opened
+"""
+
+def canUnlockAll(boxes):
+    """
+    Parameters
+        boxes (list): A list of lists representing the boxes and their keys
+
+    Return
+        bool: True if all the boxes can be unlocked, otherwise False
+    """
+
+    if not boxes:
+        return False
+
+    number_of_boxes = len(boxes)
+    visited = set()
+    visited.add(0)  # The first box is unlocked
+
+    box_stack = [0]  # Start with the first box
+    while box_stack:
+        current_box = box_stack.pop()
+
+        for key in boxes[current_box]:
+            if key < number_of_boxes and key not in visited:
+                visited.add(key)
+                box_stack.append(key)
+
+    return len(visited) == number_of_boxes

738e069314b0a4ba512f3f3c3e51c56d95b287ac
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-01 18:17:39 +0200

    A backtracking algorithm to place N non-attacking queens on an N×N chessboard.

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 """
 Program that solves the `N` queens problem using a backtracking algorithm to
@@ -8,132 +8,61 @@ place N non-attacking queens on an N x N chessboard
 import sys
 
 
-def chess_board(board):
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
+
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
+
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+
+n = int(sys.argv[1])
+
+
+def board(size, row=0, column=[], forward_diagonal=[], backward_diagonal=[]):
     """
-    Function prints an 8 x 8 chess board
+    Prints the chess board on the screen
 
     Parameters
-        board - list of list with length sys.argv[1]
+        size: int, Size of the board
+        row: int, Current row index
+        column: list, list of column indexes
+        forward_diagonal: list, list of forward diagonal indexes
+        backward_diagonal: list, list of backward diagonal indexes
     """
 
-    new_list = []
-
-    for i, row in enumerate(board):
-        value = []
-        for j, column in enumerate(row):
-            if column == 1:
-                value.append(i)
-                value.append(j)
-        new_list.append(value)
-
-    print(new_list)
-
-
-def isSafe(board, row, column, number):
-    """
-    Parameters
-        board - list of list with length sys.argv[1]
-        row - row to check if is safe doing a movement in this position
-        column - column to check if is safe doing a movement in this position
-        number: size of the board
-
-    Return
-        True of False
-    """
-
-    # Check this row in the left side
-    for i in range(column):
-        if board[row][i] == 1:
-            return False
-
-    # Check upper diagonal on left side
-    for i, j in zip(range(row, -1, -1), range(column, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    for i, j in zip(range(row, number, 1), range(column, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    return True
-
-
-def solveNQUtil(board, column, number):
-    """
-    An Auxiliar function to find the posibilities of answer
-
-    Parameters
-        board - Board to resolve
-        column - Number of column
-        number - size of the board
-
-    Return
-        All the posibilites to solve the problem
-    """
-
-    if (column == number):
-        chess_board(board)
-        return True
-    result = False
-
-    for i in range(number):
-
-        if (isSafe(board, i, column, number)):
-
-            # Place this queen in board[i][column]
-            board[i][column] = 1
-
-            # Make result true if any placement
-            # is possible
-            result = solveNQUtil(board, column + 1, number) or result
-
-            board[i][column] = 0  # BACKTRACKING
-
-    return result
-
-
-def solve(number):
-    """
-    Function finds all the posibilities if exists
-
-    Parameters
-        number - size of the board
-    """
-
-    board = [
-        [0 for i in range(number)]for i in range(number)
-        ]
-
-    if not solveNQUtil(board, 0, number):
-        return False
-
-    return True
-
-
-def validate(args):
-    """
-    Validating the input data to verify if the size to answer is possible
-
-    Parameters
-        args - sys.argv
-    """
-
-    if (len(args) == 2):
-        # Validate data
-        try:
-            number = int(args[1])
-        except Exception:
-            print("N must be a number")
-            exit(1)
-        if number < 4:
-            print("N must be at least 4")
-            exit(1)
-        return number
+    if row < size:
+        for i in range(size):
+            if i not in column and row + i not in forward_diagonal and row - i not in backward_diagonal:
+                yield from board(
+                    size, row + 1, column + [i],
+                    forward_diagonal + [row + i], backward_diagonal + [row - i]
+                    )
     else:
-        print("Usage: nqueens N")
-        exit(1)
+        yield column
 
 
-if __name__ == "__main__":
-    number = validate(sys.argv)
-    solve(number)
+def solve(size):
+    """
+    Solves the `N` queens problem using a backtracking algorithm
+
+    Parameters
+        size: int, Size of the board
+    """
+
+    k = []
+    i = 0
+    for solution in board(size, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
+
+
+solve(n)

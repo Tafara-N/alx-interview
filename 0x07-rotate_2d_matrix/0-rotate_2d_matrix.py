92b39d2919a8b46ff0d89387c29178c47148575a
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-12 06:39:54 +0200

    Program to rotate a 2D matrix by 90 degrees.

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Program to rotate a 2D matrix by 90 degrees
+Edit the matrix in-place and does not return anything
+"""
+
+def rotate_2d_matrix(matrix):
+    """
+    Function to rotate a 2D matrix by 90 degrees
+    """
+    pass

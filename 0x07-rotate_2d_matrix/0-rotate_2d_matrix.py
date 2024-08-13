a7cb9d4093ffab9934a8379207a9ba79b455a242
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-14 00:14:26 +0200

    Program to rotate a 2D matrix by 90 degrees.

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -9,4 +9,12 @@ def rotate_2d_matrix(matrix):
     """
     Function to rotate a 2D matrix by 90 degrees
     """
-    pass
+
+    number = len(matrix)
+    for i in range(number // 2):
+        for j in range(i, number - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[number - j - 1][i]
+            matrix[number - j - 1][i] = matrix[number - i - 1][number - j - 1]
+            matrix[number - i - 1][number - j - 1] = matrix[j][number - i - 1]
+            matrix[j][number - i - 1] = temp

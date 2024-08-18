2d75d7f0d13da766c77e4740a66001fe9332e382
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-18 23:43:49 +0200

    Determining the fewest number of coins needed to meet a given amount total.

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 
 """
-Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total
+Given a pile of coins of different values, determine the fewest number of coins
+needed to meet a given amount total
 """
+
 
 def makeChange(coins, total):
     """
-    Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total
+    Given a pile of coins of different values, determine the fewest number of
+    coins needed to meet a given amount total
+
+    Parameters
+        coins: list of integers
+        total: integer
+
+    Return
+        fewest number of coins needed to meet a given amount
     """
 
     pass

3a67c0b4d5ef616e1227294d202d324b46e1f087
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-29 14:23:34 +0200

    Function returns the perimeter of the island.

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Function returns the perimeter of the island
+"""
+
+def island_perimeter(grid):
+    """
+    Function returns the perimeter of the island
+
+    Parameters:
+        grid (list): list of list of integers
+
+    Returns:
+        perimeter (int): perimeter of the island
+    """
+
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
